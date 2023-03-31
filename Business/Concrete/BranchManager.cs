8fb698f789540c71af5d829888c6e681a8b219ef
Burcu Taş <burcutas636@gmail.com>
2023-03-31 10:40:55 +0300

    Business code was written for BranchManager

--- Business/Concrete/BranchManager.cs
+++ Business/Concrete/BranchManager.cs
@@ -3,6 +3,7 @@ using Business.BusinessAspects.Autofac;
 using Business.Constants.Messages.Entity;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -22,13 +23,24 @@ namespace Business.Concrete
             _branchDal = branchDal;
         }
 
+
         [SecuredOperation("admin")]
         [ValidationAspect(typeof(BranchValidator))]
         public IResult Add(Branch branch)
         {
+            var result = BusinessRules.Run(
+                CheckIfBranchNameExixts(branch.Name),
+                CheckIfBranchAddressExixts(branch.Address));
+
+            if (!result.Success)
+            {
+                return result;
+            }
+
             _branchDal.Add(branch);
             return new SuccessResult(BranchMessages.BranchAdded);
         }
+
 
         [SecuredOperation("admin")]
         public IResult Delete(Branch branch)
@@ -37,22 +49,53 @@ namespace Business.Concrete
             return new SuccessResult(BranchMessages.BranchDeleted);
         }
 
+
+        [SecuredOperation("admin")]
         public IDataResult<Branch> Get(int id)
         {
             return new SuccessDataResult<Branch>(_branchDal.Get(b => b.Id == id), BranchMessages.BranchWasBrought);
         }
 
+
+        [SecuredOperation("admin")]
         public IDataResult<List<Branch>> GetAll()
         {
             return new SuccessDataResult<List<Branch>>(_branchDal.GetAll(), BranchMessages.BranchsListed);
         }
 
 
+        [SecuredOperation("admin")]
         [ValidationAspect(typeof(BranchValidator))]
         public IResult Update(Branch branch)
         {
             _branchDal.Update(branch);
             return new SuccessResult(BranchMessages.BranchUpdated);
+        }
+
+
+
+
+        //Business Rules
+        private IResult CheckIfBranchNameExixts(string branchName)
+        {
+            var result = _branchDal.GetAll(b => b.Name == branchName).Any();
+
+            if (result)
+            {
+                return new ErrorResult(BranchMessages.BranchNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfBranchAddressExixts(string branchAddress)
+        {
+            var result = _branchDal.GetAll(b => b.Address == branchAddress).Any();
+
+            if (result)
+            {
+                return new ErrorResult(BranchMessages.BranchAddressAlreadyExists);
+            }
+            return new SuccessResult();
         }
     }
 }
